ba68169ddbcf31fc5795b0d994704b54bde82014
kelicaleb <kelicaleb7@gmail.com>
2022-11-15 12:41:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 function introduction(name){
-    var phrase = "Hi, my name is "
-    return phrase + name + "."
+    return `Hi, my name is ${name}.`
 }
 console.log(introduction("Aki"))
 console.log(introduction("Samip"))
 
 function introductionWithLanguage(name, language){
-    return "Hi, my name is " + name + " and I am learning to program in " + language + "."
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
 console.log(introductionWithLanguage("Aki", "Ember.js"))
 console.log(introductionWithLanguage("Samip", "React"))
 
 function introductionWithLanguageOptional(name, language = "JavaScript"){
-    return "Hi, my name is " + name +  " and I am learning to program in " + language + "." 
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.` )
 }
 console.log(introductionWithLanguageOptional("Gracie"))
 console.log(introductionWithLanguageOptional("Gracie", "Python"))
